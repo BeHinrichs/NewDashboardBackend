32091374d99d9b34ed1f1349dfdf33d77ead6e6c
Ben <benjamin.hinrichs@gmx.net>
2025-09-02 15:01:24 +0200

    Finish Database Change to PostgreSQL

--- task/settings.py
+++ task/settings.py
@@ -88,11 +88,17 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'dashboard_db',
         'USER': 'dashboard_user',
-        'PASSWORD': 'Py36atU98cZ;7',
+        'PASSWORD': 'Test1234',
         'HOST': 'localhost',
         'PORT': '5432',
     }
 }
+""" DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+} """
 
 
 # Password validation
