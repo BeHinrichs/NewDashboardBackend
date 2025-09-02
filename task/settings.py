33c4067c0b47221fe89ae34e5813839f2f6e6997
Ben <benjamin.hinrichs@gmx.net>
2025-09-02 14:23:08 +0200

    Change_DB

--- task/settings.py
+++ task/settings.py
@@ -85,8 +85,12 @@ WSGI_APPLICATION = 'task.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'dashboard_db',
+        'USER': 'dashboard_user',
+        'PASSWORD': 'Py36atU98cZ;7',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
